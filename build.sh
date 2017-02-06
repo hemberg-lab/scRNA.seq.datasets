3c9c6699a94120d9d20769d322e1d3d5d9972f55
Vladimir Kiselev <vladimir.yu.kiselev@gmail.com>
2017-02-06 23:29:56 +0000

    update report

--- build.sh
+++ build.sh
@@ -20,7 +20,7 @@ mkdir scater-reports
 for f in `ls scater-objects`; do
     name=(${f//./ })
     f1="'$f'"
-    Rscript -e "rmarkdown::render('report.Rmd', params = list(file = $f1))"
+    Rscript -e "rmarkdown::render('report.Rmd', params = list(file = $f1, set_title = $name))"
     mv report.html $name.html
     mv $name.html scater-reports/
 done
