81a23a1636b5976680a0423e5e8139770fc563de
Vladimir Kiselev <vladimir.yu.kiselev@gmail.com>
2017-02-06 23:39:11 +0000

    fix problem

--- build.sh
+++ build.sh
@@ -20,7 +20,8 @@ mkdir scater-reports
 for f in `ls scater-objects`; do
     name=(${f//./ })
     f1="'$f'"
-    Rscript -e "rmarkdown::render('report.Rmd', params = list(file = $f1, set_title = $name))"
+    name1="'$name'"
+    Rscript -e "rmarkdown::render('report.Rmd', params = list(file = $f1, set_title = $name1))"
     mv report.html $name.html
     mv $name.html scater-reports/
 done
