2c83aa1af069dc1bd6ebd1f2d105046923fbb9d9
Vladimir Kiselev <vladimir.yu.kiselev@gmail.com>
2017-05-12 11:16:04 +0100

    rm all docker stuff in the beginning of the build

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,13 @@
 
 WORKSPACE=$1
 
+# Delete every Docker containers
+# Must be run first because images are attached to containers
+docker rm -f $(docker ps -a -q)
+
+# Delete every Docker image
+docker rmi -f $(docker images -q)
+
 # get the docker
 docker pull quay.io/hemberg-group/scrna-seq-datasets:latest
 # run the docker
@@ -15,7 +22,3 @@ docker cp `dl`:scater-reports $WORKSPACE/
 # copy results to S3
 aws s3 cp scater-objects s3://scrnaseq-public-datasets/scater-objects/ --recursive
 aws s3 cp scater-reports s3://scrnaseq-public-datasets/scater-reports/ --recursive
-
-# cleanup after docker usage
-docker rm `dl`
-docker rmi quay.io/hemberg-group/scrna-seq-datasets
