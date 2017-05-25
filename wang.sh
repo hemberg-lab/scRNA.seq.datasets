f0bc7ed06338d0641b21153b4037bc6cbdf99407
tallulandrews <tallulandrews@gmail.com>
2017-05-25 13:07:29 +0000

    Create wang.sh
    reference: http://diabetes.diabetesjournals.org/content/65/10/3028

---
+++ wang.sh
@@ -0,0 +1,8 @@
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE83nnn/GSE83139/suppl/GSE83139_tbx-v-f-norm-ntv-cpms.csv.gz
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE83nnn/GSE83139/matrix/GSE83139-GPL11154_series_matrix.txt.gz
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE83nnn/GSE83139/matrix/GSE83139-GPL16791_series_matrix.txt.gz
+
+gunzip GSE83139_tbx-v-f-norm-ntv-cpms.csv.gz
+gunzip GSE83139-GPL11154_series_matrix.txt.gz
+gunzip GSE83139-GPL16791_series_matrix.txt.gz
+
