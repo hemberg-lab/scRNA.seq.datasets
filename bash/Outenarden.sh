2469dd228d35d55cb491c2db115eb81cb5082c08
tallulandrews <tallulandrews@gmail.com>
2017-06-02 16:00:31 +0000

    Create Outenarden.sh

---
+++ bash/Outenarden.sh
@@ -0,0 +1,10 @@
+
+#E-GEOD-76983
+#Publication: Grun et atl. (2016) De novo prediction of stem cell identity using single-cell transcriptome data. Cell Stem Cell. 19 (2) 266-277. http://dx.doi.org/10.1016/j.stem.2016.05.010
+#http://www.sciencedirect.com/science/article/pii/S1934590916300947
+#Download Link: https://www.ebi.ac.uk/arrayexpress/experiments/E-GEOD-76983/
+#Other: Mouse, bone marrow, transcript counts, CEL-Seq, bwa (align to transcriptome), short reads, 4bp UMIs (no error correction).
+
+
+wget https://www.ebi.ac.uk/arrayexpress/files/E-GEOD-76983/E-GEOD-76983.processed.1.zip
+unzip E-GEOD-76983.processed.1.zip
