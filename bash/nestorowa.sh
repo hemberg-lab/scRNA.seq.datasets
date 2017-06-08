62decad795140978d8414a6ff8f585280351c0b5
Vladimir Kiselev <vladimir.yu.kiselev@gmail.com>
2017-06-08 16:31:20 +0100

    add nestorowa files

---
+++ bash/nestorowa.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget http://blood.stemcells.cam.ac.uk/data/coordinates_gene_counts_flow_cytometry.txt.gz
+wget http://blood.stemcells.cam.ac.uk/data/all_cell_types.txt
+gunzip coordinates_gene_counts_flow_cytometry.txt.gz
