4da44964fa1f36569715ca2a8ea558554efa2cd8
Vladimir Kiselev <vladimir.yu.kiselev@gmail.com>
2017-06-09 10:31:40 +0100

    fix general script

--- general.R
+++ general.R
@@ -7,6 +7,6 @@ file <- args[1]
 sceset <- readRDS(paste0("scater-objects/", file))
 
 # remove features with duplicated names
-sceset <- sceset[!duplicated(object_ref@featureData@data$feature_symbol), ]
+sceset <- sceset[!duplicated(sceset@featureData@data$feature_symbol), ]
 
 saveRDS(sceset, paste0("scater-objects/", file))
