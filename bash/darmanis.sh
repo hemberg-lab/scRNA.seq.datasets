7912c9425e41c42791939ee14c42bf95e35b4a82
Vladimir Kiselev <vladimir.yu.kiselev@gmail.com>
2017-06-26 14:39:31 +0100

    add darmanis

--- bash/darmanis.sh
+++ bash/darmanis.sh
@@ -9,7 +9,7 @@ wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE67nnn/GSE67835/matrix/GSE67835-GPL
 gunzip GSE67835-GPL15520_series_matrix.txt.gz
 gunzip GSE67835-GPL18573_series_matrix.txt.gz
 
-perl parse_series_matrix.pl GSE67835-GPL15520_series_matrix.txt > Ann_part1.txt
+perl ../utils/parse_series_matrix.pl GSE67835-GPL15520_series_matrix.txt > Ann_part1.txt
 mv ExprMat.txt ExprMat1.txt
-perl parse_series_matrix.pl GSE67835-GPL18573_series_matrix.txt > Ann_part2.txt
+perl ../utils/parse_series_matrix.pl GSE67835-GPL18573_series_matrix.txt > Ann_part2.txt
 mv ExprMat.txt ExprMat2.txt
