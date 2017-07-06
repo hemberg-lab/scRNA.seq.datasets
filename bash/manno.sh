f53507e2838bc0ad98963b470c42dfad93f28be2
tallulandrews <tallulandrews@gmail.com>
2017-07-06 09:18:06 +0000

    Create manno.sh
    Neurons
    La Manno G, Gyllborg D, Codeluppi S, Nishimura K et al. Molecular Diversity of Midbrain Development in Mouse, Human, and Stem Cells. Cell 2016 Oct 6;167(2):566-580.e19. PMID: 27716510

---
+++ bash/manno.sh
@@ -0,0 +1,15 @@
+# Mouse (adult & embryo), Human embryo, & human ipsc-derived dopaminergic neurons
+# La Manno G, Gyllborg D, Codeluppi S, Nishimura K et al. Molecular Diversity of Midbrain Development in Mouse, Human, and Stem Cells. Cell 2016 Oct 6;167(2):566-580.e19. PMID: 27716510
+# GSE76381
+
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE76nnn/GSE76381/suppl/GSE76381_ESMoleculeCounts.cef.txt.gz
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE76nnn/GSE76381/suppl/GSE76381_EmbryoMoleculeCounts.cef.txt.gz
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE76nnn/GSE76381/suppl/GSE76381_MouseAdultDAMoleculeCounts.cef.txt.gz
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE76nnn/GSE76381/suppl/GSE76381_MouseEmbryoMoleculeCounts.cef.txt.gz
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE76nnn/GSE76381/suppl/GSE76381_iPSMoleculeCounts.cef.txt.gz
+
+gunzip GSE76381_ESMoleculeCounts.cef.txt.gz
+gunzip GSE76381_EmbryoMoleculeCounts.cef.txt.gz
+gunzip GSE76381_MouseAdultDAMoleculeCounts.cef.txt.gz
+gunzip GSE76381_MouseEmbryoMoleculeCounts.cef.txt.gz
+gunzip GSE76381_iPSMoleculeCounts.cef.txt.gz
