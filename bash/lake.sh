2d43c8635157d437d8c6095e39505a77211e2fb7
tallulandrews <tallulandrews@gmail.com>
2017-07-18 14:37:06 +0000

    Create lake.sh

---
+++ bash/lake.sh
@@ -0,0 +1,13 @@
+# http://genome-tech.ucsd.edu/ZhangLab/index.php/data/epigenomics-and-transcriptomics/sns/
+# Lake et al. (2016) Neuronal subtypes and diversity revealed by single-nucleus RNA sequencing of the human brain. Science. 352 (6293): 1586-1590
+
+#Prefrontal cortex - BA10
+#Frontal cortex (eye fields) - BA8
+#Auditory cortex (speech) - BA21
+#Auditory cortex (Wernicke's area) - BA22
+#Auditory cortex - BA41/42
+#Visual cortex - BA17
+
+wget http://genome-tech.ucsd.edu/public/Lake_Science_2016/Lake-2016_Gene_TPM.dat.gz
+wget http://genome-tech.ucsd.edu/public/Lake_Science_2016/Lake-2016_Gene_TPM_Sample-annotation.txt
+gunzip Lake-2016_Gene_TPM.dat.gz
